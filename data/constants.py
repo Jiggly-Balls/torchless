41ac3d9977b2382aefa68869e29f699fb7d62395
Jiggly Balls <jigglyballs9000@gmail.com>
2025-11-05 02:15:58 +0530

    Added paths to assets

--- data/constants.py
+++ data/constants.py
@@ -3,8 +3,14 @@ from __future__ import annotations
 __all__ = (
     "FPS",
     "TILE_SIZE",
+    "SCREEN_RESOLUTION",
+    "BASE_ASSET_PATH",
+    "GRASS_SPRITE_SHEET",
 )
 
 FPS: int = 60
 TILE_SIZE: int = 32
 SCREEN_RESOLUTION: tuple[int, int] = (1000, 600)
+
+BASE_ASSET_PATH = "assets/"
+GRASS_SPRITE_SHEET = BASE_ASSET_PATH + "grass.png"
