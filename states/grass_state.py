31a7d1332f67a305f617eb6f1837f1a96d2562b4
Jiggly Balls <jigglyballs9000@gmail.com>
2025-11-05 02:16:14 +0530

    Added update loop

--- states/grass_state.py
+++ states/grass_state.py
@@ -1,18 +1,30 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import pygame
 from game_state import State
+
+from core.utils import load_sprite_sheet
+from data.constants import GRASS_SPRITE_SHEET
 
 if TYPE_CHECKING:
     from pygame import Event
 
 
 class GrassState(State):
+    def __init__(self) -> None:
+        self.grass_sprites = load_sprite_sheet(GRASS_SPRITE_SHEET)
+        print(f"{self.grass_sprites=}")
+
     def process_event(self, event: Event) -> None:
         if event.type == pygame.QUIT:
             self.manager.is_running = False
+
+    def process_update(self, *args: Any) -> None:
+        self.window.fill((50, 50, 50))
+
+        pygame.display.update()
 
 
 def hook() -> None:
