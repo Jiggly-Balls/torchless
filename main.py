2a51b212055391a556c7354228588e1151fc43fd
Jiggly Balls <jigglyballs9000@gmail.com>
2025-11-05 00:52:18 +0530

    Initialized main screen & loaded states

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from game_state import StateManager
+
+__version__ = "0.1.0"
+
+pygame.init()
+pygame.display.init()
+pygame.display.set_caption("Torchless v" + __version__)
+
+
+def main() -> None:
+    screen = pygame.display.set_mode((1000, 600))
+    clock = pygame.time.Clock()
+
+    manager = StateManager(screen)
+    manager.connect_state_hook("states.grass_state")
+    manager.change_state("GrassState")
+
+    assert manager.current_state is not None
+
+    while manager.is_running:
+        dt = clock.tick(60) / 1000
+
+        for event in pygame.event.get():
+            manager.current_state.process_event(event)
+
+        manager.current_state.process_update(dt)
+
+
+if __name__ == "__main__":
+    main()
