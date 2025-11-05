dbbc5701c41443a6adbe0124f66f298055f57c93
Jiggly Balls <jigglyballs9000@gmail.com>
2025-11-05 18:46:45 +0530

    Added grass spawning system

--- states/grass_state.py
+++ states/grass_state.py
@@ -1,28 +1,50 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+import random
+from typing import TYPE_CHECKING
 
 import pygame
 from game_state import State
 
+from core.grass import GrassGroup, GrassSprite
 from core.utils import load_sprite_sheet
 from data.constants import GRASS_SPRITE_SHEET
 
 if TYPE_CHECKING:
-    from pygame import Event
+    from typing import Any
+
+    from pygame import Event, Surface
 
 
 class GrassState(State):
     def __init__(self) -> None:
-        self.grass_sprites = load_sprite_sheet(GRASS_SPRITE_SHEET)
-        print(f"{self.grass_sprites=}")
+        self.grass_sprites: list[Surface] = load_sprite_sheet(
+            GRASS_SPRITE_SHEET
+        )
+        self.grass_group: GrassGroup = GrassGroup()
 
     def process_event(self, event: Event) -> None:
         if event.type == pygame.QUIT:
             self.manager.is_running = False
 
+        # elif event.type == pygame.MOUSEBUTTONDOWN:
+        #     if event.button == 1:
+        #         # print(event.pos)
+
     def process_update(self, *args: Any) -> None:
         self.window.fill((50, 50, 50))
+
+        mouse_button_state = pygame.mouse.get_pressed()
+        if mouse_button_state[0]:
+            self.grass_group.add(
+                GrassSprite(
+                    self.grass_group,
+                    random.choice(self.grass_sprites),
+                    pygame.mouse.get_pos(),
+                )
+            )
+
+        self.grass_group.draw(self.window)
 
         pygame.display.update()
 
